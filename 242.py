0e0eab8fb3179f4b817cf7f116a7d45471af41fd
viza12 <viza12@users.noreply.github.com>
2015-03-11 01:25:51 +0200

    Update 242.py
    Labojam kļūdas no kopēšanas

--- 242.py
+++ 242.py
@@ -1,13 +1,21 @@
-from Tkinter import * import time
+from Tkinter import * 
+import time
+
+# Create objects
 w = Tk(); f = Frame(); f.pack()
 time_var = StringVar()
-time_labelis = Label(f, textvariable=time_var, font="Courier 60",
-bg="Black",fg="#00B000")
+time_labelis = Label(f, textvariable=time_var, font="Courier 60", bg="Black", fg="#00B000")
 time_labelis.pack()
+
+# Create function for flashing
 def mirgot():
-"""Funkcija datu atjaunoshanai""" t = time.localtime(time.time())
-if t[5] % 2: # mirgoshanas efekts fmt = "%H:%M"
-else:
-fmt = "%H %M"
-time_var.set(time.strftime(fmt, t)) time_labelis.after(500, mirgot) # gaidam 0.5sec
-time_labelis.after(500, mirgot) w.mainloop()
+    """Funkcija datu atjaunoshanai""" 
+    t = time.localtime(time.time())
+    if t[5] % 2:                        # mirgoshanas efekts fmt = "%H:%M"
+        fmt = "%H %M"
+    else:
+        time_var.set(time.strftime(fmt, t)) 
+        time_labelis.after(500, mirgot) # gaidam 0.5sec
+        
+time_labelis.after(500, mirgot) 
+w.mainloop()
